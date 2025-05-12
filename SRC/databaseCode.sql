455ae18fde685c912be3d52d569c649077a07fcd
Bruno dos Santos Oliveira <brunoso2006@gmail.com>
2025-05-12 11:07:06 -0300

    organizing repository

--- SRC/databaseCode.sql
+++ SRC/databaseCode.sql
@@ -1,11 +1,11 @@
-create database dht11_db;
-use dht11_db;
+CREATE DATABASE dht11_db;
+USE dht11_db;
 
-create table dht11_readings(
+CREATE TABLE dht11_readings(
 	id INT AUTO_INCREMENT PRIMARY KEY,
     temperature FLOAT,
     humidity FLOAT,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-select * from dht11_readings;
+SELECT * FROM dht11_readings;
