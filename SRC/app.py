2b5dc24cfd2d711b87914c2c1d040414170cdf48
Bruno dos Santos Oliveira <brunoso2006@gmail.com>
2025-06-02 15:18:12 -0300

    02062025updateErrorsCorrection

--- SRC/app.py
+++ SRC/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for
 import pymysql.cursors
 from dotenv import load_dotenv
 import os
@@ -6,6 +6,7 @@ import pymysql #Conector com o banco de dadoflask
 
 load_dotenv()
 app = Flask(__name__)
+app.secret_key = "chaveSegurança"
 
 def get_db_connection():
     connection = pymysql.connect(
@@ -41,8 +42,9 @@ def get_data():
 @app.route('/home')
 def home():
     dados = get_data()
+    user_name = request.args.get('user_name')
     print(dados)
-    return render_template("home.html", dados=dados)
+    return render_template("home.html", dados=dados, user_name=user_name)
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
@@ -52,20 +54,21 @@ def cadastro():
         cpf = request.form['cpf']
         senha = request.form['senha']
         connection = get_db_connection()
-        if connection:
-            cursor = connection.cursor(pymysql.cursors.DictCursor)
-            cursor.execute("SELECT * FROM users WHERE email = %s OR cpf = %s", (email, cpf))
-            ex = cursor.fetchone()
-            if ex:
-                return render_template('register.html', error="Email ou CPF já registrado.")
-            else:
-                cursor.execute(
-                    "INSERT INTO users (nome, email, cpf, senha) VALUES (%s, %s, %s, %s)",
-                    (nome, email, cpf, senha)
-                )
-            connection.commit()
-            return redirect(url_for('index'))
-    return render_template('register.html')
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT id FROM users WHERE email = %s OR cpf = %s", (email, cpf))
+        user = cursor.fetchone()
+
+        if user:
+            error_message = "Email ou CPF já existem."
+            return render_template("register.html", error=error_message)
+        cursor.execute("INSERT INTO users (nome, email, cpf, senha) VALUES (%s, %s, %s, %s)",
+                       (nome, email, cpf, senha))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return redirect(url_for('index', success='true'))
+
+    return render_template("register.html")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -75,13 +78,18 @@ def index():
         connection = get_db_connection()
         if connection:
             cursor = connection.cursor(pymysql.cursors.DictCursor)
-            cursor.execute("SELECT * FROM users WHERE email = %s or cpf = %s", (entrada, entrada))
+            cursor.execute("SELECT senha, email, nome FROM users WHERE email = %s or cpf = %s", (entrada, entrada))
             user = cursor.fetchone()
             if user and user['senha'] == senha:
-               return redirect(url_for('home'))
+               return redirect(url_for('home', user_name=user['nome']))
             else:
-                return render_template('index.html', error='Invalid credentials')
+                if user is None:
+                    error_message = 'Usuário não encontrado. Verifique seu email ou CPF.'
+                    return render_template('index.html', error=error_message)
+                elif user and user['senha'] != senha:
+                    error_message = 'Senha incorreta. Tente novamente.'
+                    return render_template('index.html', error=error_message)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
--- SRC/databaseCode.sql
+++ SRC/databaseCode.sql
@@ -7,17 +7,20 @@ CREATE TABLE dht11_readings(
     humidity FLOAT,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE,
+    username VARCHAR(30),
     cpf VARCHAR(11) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users(nome, email, cpf, senha)
 VALUES
-("Igor", "igor@gmail.com", "52637383829", "123");
+("Bruno", "brunoso2006@gmail.com", "39011825861", "123"),
+("Igor", "igor@email.com", "11122233344", "1234");
 
 -- SELECT * FROM dht11_readings;
 -- SELECT * FROM users;
